d69506c990b3b767b01faf58ee1c8b159e6e0ca2
Mahmoud <1mahmoud.un@gmail.com>
2025-01-24 02:05:13 +0300

    create account api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/v1/account/login', [AuthController::class, 'login']);
+Route::post('/v1/account/register', [AuthController::class, 'register']);
+Route::post('/v1/account/refresh', [AuthController::class, 'refreshToken']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/v1/account/user', [AuthController::class, 'getUser']);
+    Route::get('/v1/account/logout', [AuthController::class, 'refreshToken']);
+});
